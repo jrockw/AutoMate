e81df62a6a2f5ca6302f8ec3fe6e63054278cb6c
Parth Aggarwal <parthagg@umich.edu>
2017-12-07 15:22:15 -0500

    GUI changes

--- GUI/trialGUI.py
+++ GUI/trialGUI.py
@@ -3,15 +3,65 @@ from Tkinter import *
 import ttk
 #################
 TEXTBOX_ACTIVATED = False # flag to indicate 
+buttons = []
 
 #################
 
 # This function is called whenever a key is released
+
+def initializeResponseEntry():
+    responseEntry = Text(width=40, height=3)
+    responseEntry.insert(END, 'Type here')
+    responseEntry.grid(row=2, sticky=E)
+    responseEntry.bind('<KeyRelease>', typing) 
+    responseEntry.bind('<Button-1>', clearText)
+    return responseEntry
+
+def initializeLangBox():
+    box_value = StringVar()
+    box = ttk.Combobox(root, textvariable=box_value, 
+                            state='readonly')
+    box['value'] = ('French', 'Spanish', 'German', 'Italian')
+    box.current(0)
+    box.grid(column=0, row=0, sticky = EW)
+
+def initializeButtons():
+    option1 = Button(root, text='Enter', command=lambda: buttonPressed(option1), default=ACTIVE)    # and on button 
+    option1.grid(row=4, sticky=NW)
+
+    option2 = Button(root, text='Enter', command=lambda: buttonPressed(option2), default=ACTIVE)    # and on button 
+    option2.grid(row=4)
+
+    option3 = Button(root, text='Enter', command=lambda: buttonPressed(option3), default=ACTIVE)    # and on button 
+    option3.grid(row=4, sticky=NE)
+
+    buttons = [option1, option2, option3]
+    return buttons
+
+def setButtonTexts(suggestions):
+    global buttons
+    for i in range(0,3):
+        buttons[i]["text"] = suggestions[i]
+
+def buttonPressed(button):
+    responseEntry.insert(END, button["text"])
+    pass
+def getSuggestions(lastTwoWords):
+    lastTwoWords.append('Third word')
+    return lastTwoWords
+
 def typing(event):
-   
-    userInput = responseEntry.get("1.0",END) 
-    conclusionText.delete("1.0", END)   # delete the text in our conclusion text widget
-    conclusionText.insert(END, "What you have typed so far:"+ userInput[:-1] ) 
+    userInput = responseEntry.get("1.0",END)
+    wordArray = userInput.split() 
+
+    if(len(wordArray)>1 and userInput[-2] == ' '):
+        englishSuggestion = 
+        setButtonTexts(getSuggestions(wordArray[-2:]))
+        conclusionText.delete("1.0", END)
+        conclusionText.insert(END, userInput.split()[-2:] ) 
+        #conclusionText.insert(END, "User typed space" ) 
+        #conclusionText.delete("1.0", END)   # delete the text in our conclusion text widget
+        #conclusionText.insert(END, "What you have typed so far:"+ userInput[:-1] ) 
 
 def clearText(event):
     global TEXTBOX_ACTIVATED
@@ -19,8 +69,6 @@ def clearText(event):
     if(not TEXTBOX_ACTIVATED):
         responseEntry.delete("1.0", END)
         TEXTBOX_ACTIVATED = True
-def button1Pressed():
-    responseEntry.insert(END, 'INSERT')
 
 
 ##################
@@ -30,34 +78,15 @@ hello = Label(text="Enter your text here:")
 hello.grid(row=1, sticky=W)
 
 
-box_value = StringVar()
-box = ttk.Combobox(root, textvariable=box_value, 
-                        state='readonly')
-box['value'] = ('French', 'Spanish', 'German', 'Italian')
-box.current(0)
-box.grid(column=0, row=0)
 
+LangBox = initializeLangBox()
+responseEntry = initializeResponseEntry()
+buttons = initializeButtons()
 
-responseEntry = Text(width=40, height=3)
-responseEntry.insert(END, 'Type here')
-responseEntry.grid(row=2, sticky=E)
-responseEntry.bind('<KeyRelease>', typing) # bind responseEntry to keyboard keys being released, and have it execute the function typing when this occurs
-responseEntry.bind('<Button-1>', clearText)
-
-option1 = Button(root, text='Enter', command=lambda: button1Pressed(), default=ACTIVE)    # and on button 
-option1.grid(row=4, sticky=NW)
-
-option2 = Button(root, text='Enter', command=lambda:0, default=ACTIVE)    # and on button 
-option2.grid(row=4)
-
-option3 = Button(root, text='Enter', command=lambda:0, default=ACTIVE)    # and on button 
-option3.grid(row=4, sticky=NE)
 
 conclusionText = Text(width=40, height=5)
 conclusionText.insert(END, "What you have typed so far:")
 conclusionText.grid(row=5, columnspan=2)
-
-
 
 
 
