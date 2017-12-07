27eb3272546f0a14fbac6f1ef1c12f2251660b00
Jacob <jrockw@umich.edu>
2017-12-06 20:18:50 -0500

    Rough Python GUI

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,25 +1,29 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+/*chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
+  /*chrome.tabs.executeScript({
+    code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="<button>Fuck Me </button>";'
+  });
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+   
   });
-});
+});*/
 
 // This block is new!
-chrome.runtime.onMessage.addListener(
+/*chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
       chrome.tabs.create({"url": request.url});
     }
   }
-);
+);*/
 
 //chrome.input.ime.onFocus.addListener(function callback)
-
+/*
 var ime_api = chrome.input.ime;
 var context_id = -1;
 console.log("Initializing IME");
@@ -46,4 +50,29 @@ function(engineID, keyData) {
     return true;
   }
   return false
-});
+}); */
+// button insertion
+//chrome.browserAction.onClicked.addListener(function(tab) {
+ // chrome.tabs.executeScript(null, {file: "content.js"});
+//});
+
+
+
+
+
+
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true, 
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 
+      'tab.url should be a string');
+    callback(url);
+  });
+}
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,15 +1,18 @@
 // content.js
-alert("Hello from trAIn JackBot")
+//alert("Hello from trAIn JackBot")
 
-chrome.runtime.onMessage.addListener(
+/*chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "clicked_browser_action" ) {
-      	var firstHref = $("a[href^='http']").eq(0).attr("href");
-
+      	//var firstHref = $("a[href^='http']").eq(0).attr("href");
       	console.log(firstHref);
 
       	chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     	}
     }
   
-);
+); */
+var theElement = document.createElement("<h1> suck a dick </h1>'");
+document.getElementByID("body").appendChild(theElement);
+//<button> Fuck off </button>
+
